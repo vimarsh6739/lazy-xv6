39d3f2dd2ad71ec69fb8c122f9f322ac696d65e0
Vimarsh Sathia <vimarsh.sathia@gmail.com>
2019-11-11 21:29:14 +0530

    Added time_elapsed.c

--- time_elapsed.c
+++ time_elapsed.c
@@ -8,12 +8,12 @@ int main(void)
     int* a = (int*)malloc(sizeof(int)*1024*1024);
     uint y = get_ticks();
     printf(1,"Time taken to allocate the array is %d\n",y-x);
-    int i;
+    //int i;
     x = get_ticks();
     a[0] = 1;
     y = get_ticks();
     printf(1,"Time taken to access Page 0 is %d\n",y-x);
-    for(i = 55;i < 512*1024;i += 1024)
+    /*for(i = 55;i < 512*1024;i += 1024)
     {
         x = get_ticks();
         a[i] =1;
@@ -24,6 +24,6 @@ int main(void)
         a[i+1] =1;
         y = get_ticks();
         printf(1,"Time taken to access index %d is %d\n",i+1,y-x);
-    }
+    }*/
     exit();
 }
